f1d3762f482738cbf5307cb90e761628b4b8c0e4
Verbalist <enshteynn@ukr.net>
2014-04-07 18:08:04 +0300

    last com

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -2,13 +2,36 @@
 using System.Collections;
 
 public class Game : MonoBehaviour {
-
+	Vector3 a = new Vector3(0,0,0);
 	// Use this for initialization
 	void Start () {
-	
 	}
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (0, 2, 0);
+		if (Input.GetKey (KeyCode.A))
+		{
+			a = transform.position;
+			a.x -=0.1f;
+			transform.position = a;
+			SpriteRenderer.sprite = "fish";
+		}
+	if (Input.GetKey (KeyCode.W))
+		{
+			a = transform.position;
+			a.y +=0.1f;
+			transform.position = a;
+		}
+		if (Input.GetKey (KeyCode.D))
+		{
+			a = transform.position;
+			a.x +=0.1f;
+			transform.position = a;
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			a = transform.position;
+			a.y -=0.1f;
+			transform.position = a;
+		}
 	}
 }
