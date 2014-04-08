a5fec2dee4dcc4ef0f0fea52ce7b13c8d741531e
Verbalist <enshteynn@ukr.net>
2014-04-08 12:19:10 +0300

    test flup 1

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -2,36 +2,66 @@
 using System.Collections;
 
 public class Game : MonoBehaviour {
-	Vector3 a = new Vector3(0,0,0);
+
 	// Use this for initialization
 	void Start () {
+		isStartButtonPressed = false;
+		Time.timeScale = 0.0f;
 	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.A))
-		{
-			a = transform.position;
-			a.x -=0.1f;
-			transform.position = a;
-			SpriteRenderer.sprite = "fish";
+		updateScore ();
+		if (!isInView()) {
+			restartGame ();
 		}
-	if (Input.GetKey (KeyCode.W))
-		{
-			a = transform.position;
-			a.y +=0.1f;
-			transform.position = a;
+		if (Input.anyKeyDown) {
+			move ();
 		}
-		if (Input.GetKey (KeyCode.D))
-		{
-			a = transform.position;
-			a.x +=0.1f;
-			transform.position = a;
+	}
+	private bool isinView() {
+		Vector3 port = Camera.main.WorldToViewportPoint (transform.position);
+		if ((port.x < 1) && (port.x > 0) && (port.y < 1) && (port.y < 0) && (port.z > 0)) {
+						return true;
+		} else {
+			return false;
 		}
-		if (Input.GetKey (KeyCode.S))
-		{
-			a = transform.position;
-			a.y -=0.1f;
-			transform.position = a;
+	}
+
+	private bool isStartButtonPressed;
+	public GUIText scoreLabel1;
+
+	void OnGUI(){
+		if (isStartButtonPressed) {
+			GUI.TextField (new Rect (Screen.width / 2 - 65, Screen.height / 2 - 11, 120, 32), "Do something to start");
+			if (Input.anyKeyDown) {
+			Time.timeScale = 1.0f;
+			isStartButtonPressed = true;
+			}
+		}
+	}
+
+	private void move(){
+		rigidbody.velocity = new Vector3 (0, 0, 0);
+		rigidbody.AddForce(new Vector3(275,200,0),ForceMode.Force);
+	}
+
+	private void onTriggerEnter(Collider other)
+	{
+		restartGame ();
+	}
+
+	private void restartGame()
+	{
+		Time.timeScale = 0.0f;
+		isStartButtonPressed = false;
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+
+	private void updateScore()
+	{
+		int score = (int)(transform.position.x / GenereteWorld.distanceBetweenObjects);
+		if ((score != (int.Parse(scoreLabel.text))) && (score > 0)) {
+			scoreLabel.text = score.ToString();
 		}
 	}
 }
